16130d46e870156f1f246ce7cec6cd4d08d385fe
BuildTools <unconfigured@null.spigotmc.org>
2020-06-17 10:52:02 +0900

    Login

--- src/main/kotlin/xyz/gwcr/honran/namutree0345/MagpieBotReboot/Main.kt
+++ src/main/kotlin/xyz/gwcr/honran/namutree0345/MagpieBotReboot/Main.kt
@@ -1,4 +1,9 @@
 package xyz.gwcr.honran.namutree0345.MagpieBotReboot
 
-class Main {
+import net.dv8tion.jda.api.*
+import java.io.File
+
+fun main() {
+    var builder: JDABuilder = JDABuilder(File("./token.txt").readText())
+    val api: JDA = builder.build()
 }
