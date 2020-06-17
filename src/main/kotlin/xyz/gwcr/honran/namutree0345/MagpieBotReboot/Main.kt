22a34aaaff97d4bf07cc53fc77064fed50529de0
BuildTools <unconfigured@null.spigotmc.org>
2020-06-17 11:05:21 +0900

    Add presence & JVM target 1.8 fix

--- src/main/kotlin/xyz/gwcr/honran/namutree0345/MagpieBotReboot/Main.kt
+++ src/main/kotlin/xyz/gwcr/honran/namutree0345/MagpieBotReboot/Main.kt
@@ -1,9 +1,11 @@
 package xyz.gwcr.honran.namutree0345.MagpieBotReboot
 
 import net.dv8tion.jda.api.*
+import net.dv8tion.jda.api.entities.Activity
 import java.io.File
 
 fun main() {
     var builder: JDABuilder = JDABuilder(File("./token.txt").readText())
     val api: JDA = builder.build()
+    api.presence.activity = Activity.playing("👑 까치봇 부활!")
 }
